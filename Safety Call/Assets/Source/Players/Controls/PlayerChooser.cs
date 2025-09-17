dd3812e08d64e92f0665a42a7cda77d854300ac5
Uzel <156241117+UlzitonGit@users.noreply.github.com>
2025-09-17 21:42:42 +0300

    FieldOfViewExtra

--- Safety Call/Assets/Source/Players/Controls/PlayerChooser.cs
+++ Safety Call/Assets/Source/Players/Controls/PlayerChooser.cs
@@ -1,3 +1,4 @@
+using System;
 using Source.Creatures.Movement;
 using Source.Players.Movement;
 using UnityEngine;
@@ -14,6 +15,26 @@ namespace Source.Players.Controls
         [SerializeField] private PlayerMoveInput _playerMoveInput;
     
         [SerializeField] private PlayerSpotlighter _playerSpotlighter;
+
+        private void Update()
+        {
+            if (Input.GetKey(KeyCode.Alpha1))
+            {
+                SetChosenPlayer(0);
+            }
+            if (Input.GetKey(KeyCode.Alpha2))
+            {
+                SetChosenPlayer(1);
+            }
+            if (Input.GetKey(KeyCode.Alpha3))
+            {
+                SetChosenPlayer(2);
+            }
+            if (Input.GetKey(KeyCode.Alpha4))
+            {
+                SetChosenPlayer(3);
+            }
+        }
 
         public void SetChosenPlayer(int playerIndex)
         {
