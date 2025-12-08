3460edfb0e2cfbb0c07fa0234a2c110049272ac2
Uzel <anton.ulzibat@gmail.com>
2025-12-08 19:10:27 +0300

    MetroLight

--- Safety Call/Assets/Source/Hostage/HostagesSpawner.cs
+++ Safety Call/Assets/Source/Hostage/HostagesSpawner.cs
@@ -9,9 +9,7 @@ public class HostagesSpawner : MonoBehaviour
 
     [SerializeField] private int[] _hostagesInLocationRandomize;
 
-    private List<int> busySpawns = new List<int>();
-
-    private List<EnemyMovement> _enemyMovements = new List<EnemyMovement>();
+   
 
 
     private void Start()
@@ -20,7 +18,7 @@ public class HostagesSpawner : MonoBehaviour
 
         int enemiesCount = Random.Range(_hostagesInLocationRandomize[0], _hostagesInLocationRandomize[1]);
 
-        for (int i = 0; i < _hostagesInLocationRandomize[0] - 1; i++)
+        for (int i = 0; i < enemiesCount; i++)
         {
             int currentSpawn = Random.Range(0, _spawnPoints.Length);
 
@@ -28,7 +26,6 @@ public class HostagesSpawner : MonoBehaviour
             _hostages[i].SetActive(true);
             _hostages[i].transform.position = _spawnPoints[currentSpawn].position;
 
-            busySpawns.Add(currentSpawn);
         }
     }
 }
